5b3aca899bc09e916dd52adc1a37340fd21c8c41
guilherme vieira <guilherme.vieira@agetec.campogrande.ms.gov.br>
2024-06-12 17:14:57 -0400

    Changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
-  belongs_to :category
-
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [50, 50]
   end
+
+  has_many :stock
+  belongs_to :category
 end
